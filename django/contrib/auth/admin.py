e2c860ab385067e06002ea804a226e7d8b707ef1
Pierre Dulac <dulacpier@gmail.com>
2015-10-26 15:13:00 +0100

    Fix the admin user change reverse url name for custom user models

--- django/contrib/auth/admin.py
+++ django/contrib/auth/admin.py
@@ -148,8 +148,9 @@ class UserAdmin(admin.ModelAdmin):
                 update_session_auth_hash(request, form.user)
                 return HttpResponseRedirect(
                     reverse(
-                        '%s:auth_%s_change' % (
+                        '%s:%s_%s_change' % (
                             self.admin_site.name,
+                            user._meta.app_label,
                             user._meta.model_name,
                         ),
                         args=(user.pk,),
